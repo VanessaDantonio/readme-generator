b2242f1507586863ceb2941d9dbb19d78c79c9fc
[Vanessa D'Antonio] <[yumin.dantonio@gmail.com]>
2023-02-28 22:49:15 +0000

    add generateMarkdown content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+## Contributing
+
+${data.contributing}
+
+Contributions, issues and feature requests are welcome!
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions feel free to contact me at ${data.email} or find me on <a href="https://github.com/${data.username}">GitHub</a>
 `;
 }
 
